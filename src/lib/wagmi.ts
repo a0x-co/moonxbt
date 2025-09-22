0091b9954f28ad736de5e8ce5359ff0dc0a1877b
Daniel Beltran <beltranvarasdaniel@gmail.com>
2025-09-22 17:09:59 -0300

    changed lorem ipsum

---
+++ src/lib/wagmi.ts
@@ -0,0 +1,17 @@
+import { http, createConfig } from "wagmi";
+import { base } from "wagmi/chains";
+import { injected, metaMask, walletConnect } from "wagmi/connectors";
+
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "your-project-id";
+
+export const config = createConfig({
+  chains: [base],
+  connectors: [
+    injected(),
+    metaMask(),
+    walletConnect({ projectId }),
+  ],
+  transports: {
+    [base.id]: http(),
+  },
+}); 
